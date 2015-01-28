39c56a0dea0afba904ae9a23f93f2790ce3eb2db
Josh B <Deliquescence1@gmail.com>
2015-01-28 10:03:25 -0500

    Exp Solver

--- src/ExpressionSolve/ExpressionSolver.java
+++ src/ExpressionSolve/ExpressionSolver.java
@@ -2,24 +2,23 @@
  * Creative Commons Attribution-NonCommercial
  * https://creativecommons.org/licenses/by-nc/4.0/
  */
-
 package ExpressionSolve;
 
 /**
  *
  * @author Josh Baird
  */
-public class ExpressionSolver
-{
+public class ExpressionSolver {
+
     public ExpressionSolver(String s) {
 
     }
 
     public void setExpression(String s) {
-       
+
     }
 
-    public void solveExpression() {
-       
+    public void solve() {
+
     }
 }
