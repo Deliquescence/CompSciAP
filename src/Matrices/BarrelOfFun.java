d1f8fc5df7ddeeecd3be50486ca9f421d4e88e08
Josh B <Deliquescence@users.noreply.github.com>
2015-02-25 10:18:02 -0500

    barrel

--- src/Matrices/BarrelOfFun.java
+++ src/Matrices/BarrelOfFun.java
@@ -15,7 +15,13 @@ public class BarrelOfFun {
     private Monkey[][] barrel;
 
     public BarrelOfFun(int rows, int cols, List<Monkey> munks) {
-
+        int i = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                barrel[r][c] = munks.get(i);
+                i++;
+            }
+        }
     }
 
     public Monkey getMonkeyWithMostBannas() {
